ac728f32c4d0e0233e2660a6e795775a318dda30
iamyotav <masayotav@gmail.com>
2022-07-19 02:24:07 -0400

    update app to send templete to contact script

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { html as bug } from "./templates/bug.md";
+import { html as chore } from "./templates/chore.md";
+import { sendTemplates } from "./utils/templatesBus";
+
+const templatesMap = new Map([
+  ["chore", chore],
+  ["bug", bug],
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  const handleSelectTemplate = () => {
+    const select = selectRef.current;
+    if (select) {
+      const selected = select.options[select.selectedIndex];
+      if (templatesMap.has(selected.value)) {
+        sendTemplates({
+          title: `${selected.value} template`,
+          body: templatesMap.get(selected.value),
+        });
+      }
+    }
+  };
 
   return (
     <div className="flex flex-col justify-center min-w-max h-100 items-center">
-      <select className="focus:ring-2 focus:ring-indigo-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-300 shadow-sm mb-2">
-        <option value="1">Bug</option>
-        <option value="2">Chore</option>
-        <option value="3">more</option>
+      <select
+        ref={selectRef}
+        defaultValue="DEFAULT"
+        className="focus:ring-2 focus:ring-indigo-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md p-2 text-center ring-1 ring-slate-300 shadow-sm mb-2"
+      >
+        <option value="DEFAULT" disabled>
+          Select your option
+        </option>
+        <option value="chore">Chore</option>
+        <option value="bug">Bug</option>
       </select>
-      <button className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg hover:bg-indigo-500">
+      <button
+        onClick={handleSelectTemplate}
+        className="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg hover:bg-indigo-500"
+      >
         Set Templates
       </button>
     </div>
