bb47a0c0d46313c3478d85d26718ec64f4bb38a7
iamyotav <masayotav@gmail.com>
2022-07-19 02:23:52 -0400

    create content script to add templets

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,3 +1,28 @@
-console.log('Hello World');
 
-export{}
+import { listenForTemplate, Template } from "../utils/templatesBus";
+const keyEvent = new KeyboardEvent('keyup', {
+    bubbles: true,
+    code: 'Enter',
+    key: 'Enter',
+})
+
+console.log('Content script loaded');
+
+const setTemplateInShortcut = ({title,body}:Template):void => {
+    const storyDescriptionElement = document.querySelector('[data-binding="description"]') as HTMLTextAreaElement;
+    const storyTitleElement = document.querySelector('[data-binding="name"]') as HTMLTextAreaElement;
+
+    if(storyDescriptionElement && storyTitleElement) {
+        storyTitleElement.value = title;
+        storyDescriptionElement.value = body;
+        storyDescriptionElement.style.height = storyDescriptionElement.scrollHeight + 'px';
+        storyDescriptionElement.focus();
+    }
+}
+
+listenForTemplate((template) => {
+    console.log(template);
+    setTemplateInShortcut(template);
+})
+
+
